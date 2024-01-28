f69a59932bcfb80e1f529ca3cc7a901259380097
AlexDoljenko <doljenkoalex@gmail.com>
2024-01-28 10:59:05 +0300

    Реализовано хранение профилей в файловой системе

--- src/main/java/com/dolsoft/configserver/ConfigServer.java
+++ src/main/java/com/dolsoft/configserver/ConfigServer.java
@@ -1,6 +1,5 @@
 package com.dolsoft.configserver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -10,7 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServer {
 
-	@Autowired
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServer.class, args);
 	}
